d1a9058910e9481c07d192eb31fd00dd868d27ce
Kenny <kamador2@ggc.edu>
2024-03-16 22:46:46 -0400

    I put "Hello, Lincoln from Kenny"

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -1,7 +1,6 @@
 public class EmptyClass {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
         System.out.println("Hello, Lincoln from kenny");
+
     }
 }
