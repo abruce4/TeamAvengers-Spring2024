e97337c1ecd63a687b7aa37ea2984a7c944b5432
abruce4 <abruce4@ggc.edu>
2024-03-16 23:02:14 -0400

    Print to the console Lincoln Bruce

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -2,5 +2,6 @@ public class EmptyClass {
     public static void main(String[] args) {
         System.out.println("Hello, this is Ginette");
         System.out.println("Hello, this is Kenny");
+        System.out.println("Hello, this is Lincoln");
     }
 }
