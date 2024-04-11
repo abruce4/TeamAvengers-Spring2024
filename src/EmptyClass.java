97ea401105e1a09a6209836a738d7f5220806cc7
huyenpham25 <hpham19@ggc.edu>
2024-04-10 22:44:49 -0400

    Create a txt file for Items 1st

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -4,7 +4,7 @@ public class EmptyClass {
         System.out.println("Hello, this is Huyen Pham");
         System.out.println("Hello, this is Ginette");
         System.out.println("Hello, this is Kenny");
-        System.out.println("Hello, this is Lincoln");
+        System.out.println("Hello, thisz is Lincoln");
         System.out.println("Hello, this is Thuy Vy");
     }
 }
