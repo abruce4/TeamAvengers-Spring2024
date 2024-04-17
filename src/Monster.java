cb5eef01ae56f7b9a95af087026199f91eba031b
abruce4 <abruce4@ggc.edu>
2024-04-17 01:56:19 -0400

    Changed the map class to only create the map and put all parsing back into their respective classes (Lincoln Bruce)

---
+++ src/Monster.java
@@ -0,0 +1,149 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**Class: Monster
+ * @author Team Avengers / Lincoln Bruce
+ * @version 1.0
+ * Course: ITEC 3860 Spring 2024
+ * Written: Apr 9, 2024
+ *
+ * This class represents a monster entity within a game. Each monster object encapsulates information
+ */
+public class Monster {
+
+    //The following variables are the common attribute between all monsters
+    //Lincoln Bruce
+    private String name;
+    private String description;
+    private int health;
+    private int attack;
+    private int dexterity;
+    private int speed;
+    private int expDrop;
+    private int goldDrop;
+    private String monsterID;
+
+    //Constructor and Initialization of attributes
+    //Lincoln Bruce
+    public Monster(String name, String description, int health, int attack, int dexterity, int speed, int expDrop, int goldDrop, String monsterID) {
+        this.name = name;
+        this.description = description;
+        this.health = health;
+        this.attack = attack;
+        this.dexterity = dexterity;
+        this.speed = speed;
+        this.expDrop = expDrop;
+        this.goldDrop = goldDrop;
+        this.monsterID = monsterID;
+
+    }
+
+    //Getters and Setters
+    //Lincoln Bruce
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getExpDrop() {
+        return expDrop;
+    }
+
+    public void setExpDrop(int expDrop) {
+        this.expDrop = expDrop;
+    }
+
+    public int getGoldDrop() {
+        return goldDrop;
+    }
+
+    public void setGoldDrop(int goldDrop) {
+        this.goldDrop = goldDrop;
+    }
+
+    public String getMonsterID() {
+        return monsterID;
+    }
+
+    public void setMonsterID(String monsterID) {
+        this.monsterID = monsterID;
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    //Method to read characters from file
+    public static void readMonsters(String filePath, ArrayList<Monster> listOfMonsters) {
+        try {
+            File myCharacters = new File(filePath);
+            Scanner myReader = new Scanner(myCharacters);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] MonsterData = data.split("-");
+                String name = MonsterData[0];
+                String description = MonsterData[1];
+                int health = Integer.parseInt(MonsterData[2]);
+                int attack = Integer.parseInt(MonsterData[3]);
+                int dexterity = Integer.parseInt(MonsterData[4]);
+                int speed = Integer.parseInt(MonsterData[5]);
+                int expDrop = Integer.parseInt(MonsterData[6]);
+                int goldDrop = Integer.parseInt(MonsterData[7]);
+                String monsterID = MonsterData[8];
+                Monster monster = new Monster(name, description, health, attack, dexterity, speed, expDrop, goldDrop, monsterID);
+                listOfMonsters.add(monster);
+
+
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred with the characters file.");
+            e.printStackTrace();
+        }
+    }
+}
